3ca43eb21a067f0b6acd9d37d78cde0063cf702a
rockysantiago <raphael.santiago@icloud.com>
2018-09-13 14:10:33 +0800

    first commit

---
+++ src/components/Header/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+import { HeaderWrapper } from './style';
+
+const Header = () => <HeaderWrapper>Header</HeaderWrapper>;
+
+export default Header;
---
+++ src/pages/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Link } from 'gatsby';
+
+import Header from '../components/Header';
+
+const IndexPage = () => (
+  <div>
+    <Header />
+    <h1>Hi people</h1>
+    <p>Welcome to your new Gatsby site.</p>
+    <p>Now go build something great.</p>
+    <Link to="/page-2/">Go to page 2</Link>
+  </div>
+);
+
+export default IndexPage;
